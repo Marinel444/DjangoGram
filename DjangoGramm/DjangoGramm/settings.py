9773a6462c9ea9cb2c51f77c0201b3285da34e6b
Marinel Marina <marinelmarina@Marinel.local>
2023-07-21 17:53:24 +0300

    settings

--- DjangoGramm/DjangoGramm/settings.py
+++ DjangoGramm/DjangoGramm/settings.py
@@ -12,14 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_effkp$nxyxr(ug*5)3=ada7lb36yvpdqz8752y752x=ah@1ya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 # for deploy
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['92.119.231.149']
-CSRF_TRUSTED_ORIGINS = ['http://92.119.231.149']
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# ALLOWED_HOSTS = ['92.119.231.149']
+# CSRF_TRUSTED_ORIGINS = ['http://92.119.231.149']
 
 # Application definition
 
